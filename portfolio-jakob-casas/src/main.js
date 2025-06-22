81cb175389296fa5ce6afc0abb688d15a7723c13
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-06-22 14:44:59 -0700

    entering transition animation on home page. top nav services modal added

--- portfolio-jakob-casas/src/main.js
+++ portfolio-jakob-casas/src/main.js
@@ -1,3 +1,46 @@
+//service nav button functionality
+const serviceButton = document.getElementById('service-button');
+const serviceModal = document.querySelector('.services-modal');
+
+serviceButton.addEventListener('mouseenter', () => {
+    serviceModal.classList.add('show');
+});
+
+serviceButton.addEventListener('mouseleave', (e) => {
+    if (!serviceModal.contains(e.relatedTarget)) {
+        serviceModal.classList.remove('show');
+    }
+});
+
+serviceModal.addEventListener('mouseenter', () => {
+    serviceModal.classList.add('show');
+});
+
+serviceModal.addEventListener('mouseleave', (e) => {
+    if (!serviceModal.contains(e.relatedTarget)) {
+        serviceModal.classList.remove('show');
+    }
+});
+
+
+//make the about me brief blob graphic move with cursor
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+    threshold: 0.3,
+    rootMargin: '0px 0px -30% 0px'
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+        observer.observe(el);
+    })
+})
+
 //setting text for about me description
 fetch('src/text/about-me-description.txt')
     .then(response => response.text())
@@ -17,3 +60,4 @@ fetch('src/text/about-me-description.txt')
         })
     })
     .catch(error => console.error('Error loading description', error));
+
