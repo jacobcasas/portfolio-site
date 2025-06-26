5e3d8fe31b246f1c6cf205ae8f7d027ff8db1ebe
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-06-25 21:03:53 -0700

    animation made for skill card as a scrolling initiative

--- src/main.js
+++ src/main.js
@@ -133,6 +133,25 @@ const showServiceLinks = () => {
     });
 };
 
+//skill card nudge
+const skillCardsMobile = document.querySelector('.skill-card-mobile');
+
+const cardObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            setTimeout(() => {
+                skillCardsMobile.classList.add('nudge');
+
+                observer.unobserve(skillCardsMobile);
+            }, 2000);
+        }
+    })
+}, {
+    threshold: 1
+});
+
+cardObserver.observe(skillCardsMobile);
+
 //setting text for about me description
 fetch('src/text/about-me-description.txt')
     .then(response => response.text())
